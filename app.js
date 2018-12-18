a4a5d5038758fa364afcca71a258012ec9f8d697
jxinchao <425775961@qq.com>
2018-12-18 13:47:54 +0800

    ...

--- app.js
+++ app.js
@@ -7,12 +7,33 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 const app = express();
+
+//设置跨域访问
+app.all('*', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+   res.header("X-Powered-By",' 3.2.1');
+   res.header("Content-Type", "application/json;charset=utf-8");
+   next();
+});
+
+
+const options = {
+  key: fs.readFileSync('ssl/1561172_live.itcnyz.com.key'),
+  cert: fs.readFileSync('ssl/1561172_live.itcnyz.com.pem')
+};
+
+
 
 app.use(bodyParser.json());
 app.use('/wechat', require('./routers/wechat'));
 app.use('/pili', require('./routers/pili'));
 
-const server = http.createServer(app);
+const server = https.createServer(options, app)
+// const server = http.createServer(app);
 
 module.exports = server;
--- index.js
+++ index.js
@@ -7,21 +7,21 @@
 const app = require('./app');
 
 const mongoose = require('mongoose');
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('ssl/1561172_live.itcnyz.com.key'),
-  cert: fs.readFileSync('ssl/1561172_live.itcnyz.com.pem')
-};
-
-https.createServer(options, function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  //console.log('listening on 8686')
-  //res.end('Hello World\n');
-}).listen(8686);
+// const https = require('https');
+// const fs = require('fs');
+//
+// const options = {
+//   key: fs.readFileSync('ssl/1561172_live.itcnyz.com.key'),
+//   cert: fs.readFileSync('ssl/1561172_live.itcnyz.com.pem')
+// };
+//
+// https.createServer(options, function(req, res) {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   console.log('listening on 8686')
+//   res.end('Hello World\n');
+// }).listen(8686)
 
 mongoose.connect('mongodb://127.0.0.1:27017/newDatabase',{useNewUrlParser:true}).catch(console.log);
 
-// app.listen(8686, () => { console.log('listening on 8686') });
+app.listen(8686, () => { console.log('listening on 8686') });
